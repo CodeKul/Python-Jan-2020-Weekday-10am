0def64ca210a7741aa0cdfaa46a7d738ff2a697d
Varun Kudalkar <varun.kudalkar@gmail.com>
2020-01-10 11:05:06 +0530

    variable and data types

---
+++ Demo.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main() {
+
+    printf("Welcome to codekul!\n");
+    return 0;
+}
---
+++ VariableAndDataTypes.py
@@ -0,0 +1,16 @@
+a = 10
+print(a)
+print(type(a))
+
+a = 10.22
+print(a)
+print(type(a))
+
+a = "Codekul"
+a = 'The Gurukul for Coders!'
+print(a)
+print(type(a))
+
+a = True
+print(a)
+print(type(a))
