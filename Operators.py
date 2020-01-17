3262f017e7f9a59e0395b10920082290cd431579
Varun Kudalkar <varun.kudalkar@gmail.com>
2020-01-17 10:43:37 +0530

    operators demo

---
+++ Operators.py
@@ -0,0 +1,56 @@
+# Assignment
+
+a = 10
+b = 20
+
+# Arithmetic
+'''
+    +
+    -
+    *
+    /
+    %
+    **
+'''
+
+res = a ** 2
+
+
+# Compound Assignment
+'''
+    +=
+    -=
+    *=
+    /=
+    %=
+    **=
+'''
+
+res += a    # res = res + a
+
+# Comparision
+'''
+    <
+    >
+    <=
+    >=
+    !=
+    ==
+'''
+
+a = 20
+res = a == b
+
+res += a
+
+# Logical
+'''
+    and
+    or
+    not
+'''
+
+res = not (a < 10 or b < 30)
+
+
+print('Result:',res)
